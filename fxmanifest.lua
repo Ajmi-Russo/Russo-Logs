ecf8f4386961a2930b9e04909afe3fcfbed6c3a7
Ajmi Russo <francohf4@gmail.com>
2026-02-14 16:01:30 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,48 @@
+--[[╔══════════════════════════════════════════════════════════╗
+    ║                                                          ║
+    ║        ██████╗ ██╗   ██╗███████╗███████╗ ██████╗         ║
+    ║        ██╔══██╗██║   ██║██╔════╝██╔════╝██╔═══██╗        ║
+    ║        ██████╔╝██║   ██║███████╗███████╗██║   ██║        ║
+    ║        ██╔══██╗██║   ██║╚════██║╚════██║██║   ██║        ║
+    ║        ██║  ██║╚██████╔╝███████║███████║╚██████╔╝        ║
+    ║        ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝ ╚═════╝         ║
+    ║                                                          ║
+    ║                Ajmi Russo Discord Bot                    ║
+    ║                  Version 1.0.2 (v13)                     ║
+    ║                  Powered by @ajmi_russo.                 ║
+    ║                                                          ║
+    ╚══════════════════════════════════════════════════════════╝ --]]
+
+fx_version 'cerulean'
+version '3.1.4'
+author 'Russo AP'
+description 'FXServer Logs By Russo AP'
+repository ' '
+
+
+dependency 'yarn'
+dependency 'screenshot-basic'
+
+-- Server Scripts
+server_scripts {
+    'server/explosions.lua',
+    'server/main.lua',
+    'server/functions.lua',
+    'server/commands.lua',
+    'server/txAdminEvents.lua',
+    'index.js'
+}
+
+--Client Scripts
+client_scripts {
+    'client/clientTables.lua',
+    'client/main.lua'
+}
+
+files {
+    'lang/*.json'
+}
+
+lua54 'yes'
+game 'gta5'
+
